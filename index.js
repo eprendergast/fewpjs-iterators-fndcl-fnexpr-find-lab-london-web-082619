aed5b89af19a792e66d7f6cb0bd59c64dce5e32e
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-10-16 09:32:37 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const testVar = {}
+const returnVar = {}
 
-function testFunc() {
-  return "hi"
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
+
+function superbowlWin(array){
+  ;
+  if (winningYear == true){
+    return winningYear.year;
+  } else {
+    return undefined;
+  }
 }
+
+function checkForWin(object){
+  if (object.result == "W"){
+    return object.year;
+  } else {
+    return undefined;
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('Hi')
+
+
+
+  superbowlWin(record);
+
+
+
+})
